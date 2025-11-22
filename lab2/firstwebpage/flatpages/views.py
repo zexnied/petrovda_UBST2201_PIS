179f18781de15a301a40f7de1a96fa9e0696736b
petrovda <111049652+zexnied@users.noreply.github.com>
2025-11-22 12:04:10 +0300

    Add files via upload

---
+++ lab2/firstwebpage/flatpages/views.py
@@ -0,0 +1,9 @@
+﻿from django.shortcuts import render
+from django.http import HttpResponse
+from django import template
+
+
+def home(request):
+    return render(request, 'templates/index.html')
+def static_handler(request):
+    return render(request, 'static_handler.html')
